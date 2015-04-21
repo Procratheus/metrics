a978e8611fa6c94f58f81c8434e94d5c79bec300
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-21 23:54:24 +0100

    fixed omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :ensure_signup_complete, only: [:new, :create, :update, :destroy]
+
+  protected
+
+  def ensure_signup_complete
+    return if action_name == "finish_signup"
+
+    if current_user && !current_user.email_verified?
+      redirect_to finish_signup_path(current_user)
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :email]
