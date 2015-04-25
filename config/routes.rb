4e13f2ca1e846be3bdd62c247a111ad7b8a3ef75
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-25 10:51:32 +0100

    api routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  namespace :api, defaults: { format: :json } do 
+    resources :events, only: [:create]
+  end
   root 'welcome#index'
   resources "registrated_applications"
 end
