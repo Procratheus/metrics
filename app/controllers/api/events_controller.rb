1b08fd573a84b727ba0bc1003eb9d6b7620528f1
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-26 13:41:43 +0100

    created

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,12 +2,13 @@ class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @event = RegistratedApplication.find_by(url: request.env["HTTP_ORIGIN"])
-    render json: "Unregistered Application", status: :unprocessable_entity unless @event
-    if registered_application.create(event_params)
+    @registered_application = RegistratedApplication.find_by(url: request.env["HTTP_ORIGIN"])
+    render json: "Unregistered Application", status: :unprocessable_entity unless @registered_application != nil
+    @event = @registered_application.events.build(event_params)
+    if @event
       render json: @event, status: :created
     else
-      reader json: @event.errors, status: :unprocessable_entity
+      render json: @event.errors, status: :unprocessable_entity
     end
   end
 
