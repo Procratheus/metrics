2f4bd3b777bf600823c59d0c69530d524b994085
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-26 14:55:29 +0100

    created event using curl

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,11 +1,12 @@
 class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
   def create
     @registered_application = RegistratedApplication.find_by(url: request.env["HTTP_ORIGIN"])
     render json: "Unregistered Application", status: :unprocessable_entity unless @registered_application != nil
     @event = @registered_application.events.build(event_params)
-    if @event
+    if @event.save
       render json: @event, status: :created
     else
       render json: @event.errors, status: :unprocessable_entity
